c18e7d1373e029fea1b69c978cf9dd528c33d8ac
yc <ysim@cs.cmu.edu>
2017-04-28 13:17:05 +0800

    Multiclass classification

--- helloworld/classifiers.py
+++ helloworld/classifiers.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from sklearn.svm import SVC
 
-from ycml.classifiers import BinaryLabelsClassifier, MultiLabelsClassifier
+from ycml.classifiers import BinaryLabelsClassifier, MultiLabelsClassifier, MulticlassLabelsClassifier
 
 __all__ = ['HelloWorldBinaryLabelsClassifier', 'HelloWorldMultiLabelsClassifier', 'CLASSIFERS_MAP']
 
@@ -39,7 +39,27 @@ class HelloWorldMultiLabelsClassifier(MultiLabelsClassifier):
 #end class
 
 
+class HelloWorldMulticlassClassifier(MulticlassLabelsClassifier):
+    def fit_binarized(self, X_featurized, Y_binarized, **kwargs):
+        self.classifier_ = SVC(probability=True).fit(X_featurized, Y_binarized)
+
+        return self
+    #end def
+
+    def _predict_proba(self, X_featurized, **kwargs):
+        return self.classifier_.predict_proba(X_featurized, **kwargs)
+        # Y_proba = np.zeros((X_featurized.shape[0], len(self.classes_)), dtype=np.float)
+
+        # for j, c in enumerate(self.classes_):
+        #     Y_proba[:, j] = self.classifiers_[c].predict_proba(X_featurized, **kwargs)[:, 1]
+
+        # return Y_proba
+    #end def
+#end class
+
+
 CLASSIFERS_MAP = {
     'HelloWorldBinaryLabelsClassifier': HelloWorldBinaryLabelsClassifier,
     'HelloWorldMultiLabelsClassifier': HelloWorldMultiLabelsClassifier,
+    'HelloWorldMulticlassClassifier': HelloWorldMulticlassClassifier,
 }
--- ycml/classifiers/labels.py
+++ ycml/classifiers/labels.py
@@ -3,11 +3,12 @@ import logging
 import numpy as np
 
 from sklearn.preprocessing import label_binarize
+from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MultiLabelBinarizer
 
 from .base import BaseClassifier
 
-__all__ = ['LabelsClassifier', 'BinaryLabelsClassifier', 'MultiLabelsClassifier']
+__all__ = ['LabelsClassifier', 'BinaryLabelsClassifier', 'MultiLabelsClassifier', 'MulticlassLabelsClassifier']
 
 logger = logging.getLogger(__name__)
 
@@ -93,6 +94,105 @@ class BinaryLabelsClassifier(LabelsClassifier):
 #end def
 
 
+class MulticlassLabelsClassifier(LabelsClassifier):
+    def __init__(self, include=[], exclude=[], **kwargs):
+        super(MulticlassLabelsClassifier, self).__init__(**kwargs)
+
+        self.exclude = set(exclude)
+        self.include = set(include)
+    #end def
+
+    def _fit(self, X, Y_labels, binarize_args={}, fit_args={}, **kwargs):
+        if self.exclude or self.include:
+            Y_labels_filtered = np.empty(Y_labels.shape, dtype=np.object)
+            removed_labels = 0
+            for i in range(Y_labels.shape[0]):
+                Y_labels_filtered[i] = [l for l in Y_labels[i] if l in self.include and l not in self.exclude]
+                removed_labels += len(Y_labels[i]) - len(Y_labels_filtered[i])
+            #end for
+            logger.info('{} label-instances removed from the training data.'.format(removed_labels))
+        else: Y_labels_filtered = Y_labels
+
+        Y_labels_filtered = np.array([Y_labels_filtered[i][0] if Y_labels_filtered[i] else '<none>' for i in range(Y_labels_filtered.shape[0])])
+
+        self.label_encoder_ = LabelEncoder().fit(Y_labels_filtered)
+        logger.info('{} labels found in training instances.'.format(len(self.classes_)))
+
+        if not len(self.classes_): raise ValueError('There are no labels available for fitting model.')
+
+        return super(MulticlassLabelsClassifier, self)._fit(X, Y_labels, binarize_args, fit_args)
+    #end def
+
+    def predict_and_proba(self, X_featurized, binarized=True, **kwargs):
+        Y_proba = self.predict_proba(X_featurized, **kwargs)
+        Y_predict_binarized = np.argmax(Y_proba, axis=1)
+
+        if binarized: return Y_proba, Y_predict_binarized
+
+        return Y_proba, self.unbinarize_labels(Y_predict_binarized)
+    #end def
+
+    def predict_proba(self, X_featurized, **kwargs):
+        if 'thresholds' in kwargs: logger.warning('thresholds argument is ignored by <{}>.'.format(self))
+        if 'denominators' in kwargs: logger.warning('denominators argument is ignored by <{}>.'.format(self))
+        if 'rescale' in kwargs:
+            del kwargs['rescale']
+            logger.warning('rescale argument is ignored by <{}>.'.format(self))
+        #end if
+
+        return super(MulticlassLabelsClassifier, self).predict_proba(X_featurized, rescale=False, **kwargs)
+    #end def
+
+    def binarize_labels(self, Y_labels):
+        classes_ = set(self.classes_)
+
+        Y_labels_filtered = np.empty(Y_labels.shape[0], dtype=np.object)
+        for i in range(Y_labels.shape[0]):
+            labels = list(filter(classes_.__contains__, Y_labels[i]))
+            Y_labels_filtered[i] = labels[0] if labels else '<none>'
+
+        return self.label_encoder_.transform(Y_labels_filtered)
+    #end def
+
+    def binarize_dicts(self, Y_dicts, default=0.0):
+        binarized = np.fill((Y_dicts.shape[0], len(self.classes_)), default, dtype=np.float)
+        classes_map = dict((c, i) for i, c in enumerate(self.clsases_))
+
+        for i in range(Y_dicts.shape[0]):
+            d = Y_dicts[i]
+            for k, p in d.items():
+                try: binarized[i, classes_map[k]] = p
+                except IndexError: pass
+            #end for
+        #end for
+
+        return binarized
+    #end def
+
+    def unbinarize_labels(self, Y_proba, epsilon=0.0, to_dict=False):
+        unbinarized = np.empty(Y_proba.shape[0], dtype=np.object)
+        if len(Y_proba.shape) == 1:
+            for i in range(Y_proba.shape[0]):
+                if to_dict: unbinarized[i] = dict([(self.classes_[Y_proba[i]], Y_proba[i])])
+                else: unbinarized[i] = [self.classes_[Y_proba[i]]]
+            #end for
+        else:
+            Y_argmax = np.argmax(Y_proba, axis=1)
+            for i in range(Y_proba.shape[0]):
+                if to_dict: unbinarized[i] = dict((self.classes_[j], Y_proba[i, j]) for j in range(Y_proba.shape[1]) if Y_proba[i, j] > epsilon)
+                else: unbinarized[i] = [self.classes_[Y_argmax[i]]]
+            #end for
+        #end if
+
+        return unbinarized
+    #end def
+
+    @property
+    def classes_(self):
+        return self.label_encoder_.classes_
+#end def
+
+
 class MultiLabelsClassifier(LabelsClassifier):
     def __init__(self, n_jobs=1, include=[], exclude=[], **kwargs):
         super(MultiLabelsClassifier, self).__init__(**kwargs)
@@ -122,11 +222,6 @@ class MultiLabelsClassifier(LabelsClassifier):
 
     def binarize_labels(self, Y_labels):
         classes_ = set(self.classes_)
-        missing = set(c for labels in Y_labels for c in labels) - classes_
-        if missing:
-            logger.debug('The following labels are ignored by the classifier: [{}]'.format(', '.join(sorted(missing))))
-            logger.warning('{} labels will be ignored by the classifier.'.format(len(missing)))
-        #end if
 
         return self.label_binarizer_.transform((filter(classes_.__contains__, labels) for labels in Y_labels))
     #end def
