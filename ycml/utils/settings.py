a561b04772de9bdd0c4b3103d97b3c26156841ac
yc <ysim@cs.cmu.edu>
2017-06-15 10:02:34 +0800

    Added force_format and use **kwargs

--- ycml/utils/settings.py
+++ ycml/utils/settings.py
@@ -10,10 +10,12 @@ __all__ = ['load_dictionary_from_file', 'save_dictionary_to_file', 'get_settings
 logger = logging.getLogger(__name__)
 
 
-def load_dictionary_from_file(f, title='dictionary'):
+def load_dictionary_from_file(f, *, force_format=None, title='dictionary'):
     if isinstance(f, str): f = uri_open(f, 'rb')
 
-    _, ext = os.path.splitext(f.name.lower())
+    if force_format: ext = '.' + force_format
+    else: _, ext = os.path.splitext(f.name.lower())
+
     if ext == '.json': o = json.load(f)
     elif ext == '.yaml': o = yaml.load(f)
     else: raise ValueError('<{}> is an unrecognized format for dictionary. Only JSON and YAML are supported right now.')
@@ -24,13 +26,22 @@ def load_dictionary_from_file(f, title='dictionary'):
 #end def
 
 
-def save_dictionary_to_file(f, d, title='dictionary', **kwargs):
+def save_dictionary_to_file(f, d, *, force_format=None, title='dictionary', **kwargs):
     if isinstance(f, str): f = open(f, 'w')
 
-    _, ext = os.path.splitext(f.name.lower())
-    if ext == '.json': json.dump(d, f, indent=4, sort_keys=True)
-    elif ext == '.yaml': yaml.dump(d, f, default_flow_style=False)
-    else: raise ValueError('<{}> is an unrecognized format for dictionary. Only JSON and YAML are supported right now.')
+    if force_format: ext = '.' + force_format
+    else: _, ext = os.path.splitext(f.name.lower())
+
+    if ext == '.json':
+        kwargs.setdefault('indent', 4)
+        kwargs.setdefault('sort_keys', True)
+        json.dump(d, f, **kwargs)
+
+    elif ext == '.yaml':
+        kwargs.setdefault('default_flow_style', False)
+        yaml.dump(d, f, **kwargs)
+
+    else: raise ValueError('<{}> is an unrecognized format for {}. Only JSON and YAML are supported right now.'.format(title))
 
     logger.info('Saved {} {} to <{}>.'.format(ext[1:].upper(), title, f.name))
 #end def
