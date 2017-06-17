a95664f75c97d847b3a5c00c6528507e9ed776f1
yc <ysim@cs.cmu.edu>
2017-06-17 20:07:43 +0800

    Debug printing

--- ycml/classifiers/labels.py
+++ ycml/classifiers/labels.py
@@ -27,6 +27,7 @@ class LabelsClassifier(BaseClassifier):
         if validation_data is not None:
             X_validation, Y_validation = validation_data
             validation_data = (X_validation, self.binarize_labels(Y_validation, **binarize_args))
+            print(self.binarize_labels(Y_validation, **binarize_args))
             logger.debug('Binarized validation labels.')
         #end if
 
