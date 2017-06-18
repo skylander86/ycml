51c29b291653ce267b8a36ce5ff7dd0564fc7737
yc <ysim@cs.cmu.edu>
2017-06-18 14:48:21 +0800

    Cleaned up and standardized

--- ycml/featclass/base.py
+++ ycml/featclass/base.py
@@ -8,7 +8,7 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from ..classifiers import load_classifier, get_thresholds_from_file
 from ..featurizers import load_featurizer
 
-from ..utils.uriutils import uri_open
+from ..utils import uri_open, get_settings
 
 __all__ = ['FeaturizerAndClassifier']
 
@@ -16,13 +16,10 @@ logger = logging.getLogger(__name__)
 
 
 class FeaturizerAndClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, featurizer_uri=None, classifier_uri=None, thresholds_uri=None, **kwargs):
-        self.featurizer_uri = featurizer_uri
-        self.classifier_uri = classifier_uri
-        self.thresholds_uri = thresholds_uri
-
-        if not self.featurizer_uri: raise Exception('Featurizer URI is not set!')
-        if not self.classifier_uri: raise Exception('Classifier URI is not set!')
+    def __init__(self, settings={}, check_environment=True, featurizer_uri=None, classifier_uri=None, thresholds_uri=None, **kwargs):
+        self.featurizer_uri = get_settings(key='featurizer_uri', sources=('env', settings), raise_on_missing=True) if featurizer_uri is None else featurizer_uri
+        self.classifier_uri = get_settings(key='classifier_uri', sources=('env', settings), raise_on_missing=True) if classifier_uri is None else classifier_uri
+        self.thresholds_uri = get_settings(key='thresholds_uri', sources=('env', settings), raise_on_missing=False) if thresholds_uri is None else thresholds_uri
 
         with uri_open(self.featurizer_uri, 'rb') as f:
             self.featurizer_ = load_featurizer(f)
@@ -50,44 +47,29 @@ class FeaturizerAndClassifier(BaseEstimator, ClassifierMixin):
         return self.predict_proba(X, **kwargs)
     #end def
 
-    def predict(self, X, binarized=False, **kwargs):
+    def predict(self, X, **kwargs):
         X_featurized = self.featurizer_.transform(X)
-        Y_predict_binarized = self.classifier_.predict(X_featurized, thresholds=self.thresholds_, **kwargs)
-        if binarized: return Y_predict_binarized
+        Y_predict = self.classifier_.predict(X_featurized, thresholds=self.thresholds_, denominators=self.denominators_, **kwargs)
 
-        return self.classifier_.unbinarize_labels(Y_predict_binarized)
+        return Y_predict
     #end def
 
     def predict_and_proba(self, X, **kwargs):
         X_featurized = self.featurizer_.transform(X)
-        return self.classifier_.predict_and_proba(X_featurized, self.thresholds_)
+        return self.classifier_.predict_and_proba(X_featurized, thresholds=self.thresholds_, denominators=self.denominators_, **kwargs)
     #end def
 
-    def predict_proba(self, X, rescale=False, **kwargs):
+    def predict_proba(self, X, **kwargs):
+        print(X)
         X_featurized = self.featurizer_.transform(X)
+        print(X_featurized)
         Y_proba = self.classifier_.predict_proba(X_featurized, **kwargs)
-
-        if rescale: return self.rescale_proba(Y_proba)
 
         return Y_proba
     #end def
 
-    def _rescale_proba(self, Y_proba):
-        n_classes = len(self.classifier_.classes_)
-
-        rescaled = np.zeros(Y_proba.shape)
-        for i in range(Y_proba.shape[0]):
-            for k in range(n_classes):
-                if Y_proba[i, k] > self.thresholds_[k]: rescaled[i, k] = 0.5 + ((Y_proba[i, k] - self.thresholds_[k]) / self.denominators_[k])
-                else: rescaled[i, k] = Y_proba[i, k] / (self.thresholds_[k] + self.thresholds_[k])
-            #end for
-        #end for
-
-        return rescaled
-    #end def
-
-    def decision_function(self, *args, **kwargs):
-        return self.predict_proba(*args, **kwargs)
+    def decision_function(self, X, **kwargs):
+        return self.predict_proba(X, **kwargs)
 
     def __str__(self):
         return 'FeaturizerAndClassifier(featurizer_uri={}, classifier_uri={}, thresholds_uri={})'.format(self.featurizer_uri, self.classifier_uri, self.thresholds_uri)
