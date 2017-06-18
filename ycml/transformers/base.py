7a93f8c3b65409460322dd90ba9011637c4c1c4a
yc <ysim@cs.cmu.edu>
2017-06-18 15:11:15 +0800

    Fix np array issues again

--- ycml/transformers/base.py
+++ ycml/transformers/base.py
@@ -14,12 +14,11 @@ logger = logging.getLogger(__name__)
 
 # Helper class. A transformer that only does transformation and does not need to fit any internal parameters.
 class PureTransformer(BaseEstimator, TransformerMixin):
-    FORCE_NP_1D_ARRAY = False
-
-    def __init__(self, nparray=True, **kwargs):
+    def __init__(self, nparray=True, nparray_dtype=None, **kwargs):
         super(PureTransformer, self).__init__(**kwargs)
 
         self.nparray = nparray
+        self.nparray_dtype = nparray_dtype
     #end def
 
     def fit(self, X, y=None, **fit_params): return self
@@ -28,10 +27,14 @@ class PureTransformer(BaseEstimator, TransformerMixin):
         timer = Timer()
         transformed = self._transform(X, **kwargs)
         if self.nparray:
-            if self.FORCE_NP_1D_ARRAY: transformed = np.array(transformed, dtype=np.object)
-            else: transformed = np.array(transformed)
-            if transformed.ndim == 1:
-                transformed = transformed.reshape(transformed.shape[0], 1)
+            nparray_dtype = getattr(self, 'nparray_dtype', None)
+            if nparray_dtype:
+                transformed = np.array(transformed, dtype=nparray_dtype)
+            else:
+                transformed = np.array(transformed)
+                if transformed.ndim == 1:
+                    transformed = transformed.reshape(transformed.shape[0], 1)
+            #end if
         #end if
         logger.debug('Done <{}> transformation{}.'.format(type(self).__name__, timer))
 
