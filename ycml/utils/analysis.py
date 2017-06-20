d7dc0241c555cf4671cd7799de5a0fda391a6d6a
yc <ysim@cs.cmu.edu>
2017-06-20 17:52:16 +0800

    Debug

--- ycml/utils/analysis.py
+++ ycml/utils/analysis.py
@@ -27,6 +27,7 @@ def classification_report(Y_true, Y_proba, *, labels=None, target_names=None, th
 
     if isinstance(thresholds, float): thresholds = np.full(n_classes, thresholds)
     if thresholds is not None and n_classes == 2: thresholds[1] = 1.0 - thresholds[0]
+    print(thresholds)
     assert thresholds is None or ((thresholds <= 1).all() and (thresholds >= 0.0).all())
 
     if isinstance(precision_thresholds, float): precision_thresholds = np.full(n_classes, precision_thresholds)
