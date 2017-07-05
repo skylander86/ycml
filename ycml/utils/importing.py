5fafc0ff83a0f7335155e842baad663e5ec9347a
yc <yanchuan@outlook.com>
2017-07-05 18:48:53 +0800

    Fix typo

--- ycml/utils/importing.py
+++ ycml/utils/importing.py
@@ -5,7 +5,7 @@ from importlib import import_module
 
 def get_class_from_module_path(path):
     try: module_path, class_name = path.rsplit('.', 1)
-    except ValueError: raise ImportError('{} is not a valid module path. You need to specify the full Python dotted path to the module.')
+    except ValueError: raise ImportError('{} is not a valid module path. You need to specify the full Python dotted path to the module.'.format(path))
 
     module = import_module(module_path)
     klass = getattr(module, class_name)
