3c8496c226c6120ac856c9a354f5ad25a8f8d470
yc <yanchuan@outlook.com>
2017-07-05 18:48:37 +0800

    gAdd display thresholding

--- ycml/utils/instances.py
+++ ycml/utils/instances.py
@@ -15,7 +15,26 @@ from .timer import Timer
 logger = logging.getLogger(__name__)
 
 
-def load_instances(instance_files, labels_field='labels', limit=None, progress_interval=None):
+def load_instances(instance_files, labels_field='labels', display_threshold=None, limit=None, progress_interval=None):
+    if not isinstance(instance_files, (list, tuple)): instance_files = [instance_files]
+
+    def _filter_display(d):
+        if display_threshold is None: return d
+
+        ignored_count, ignored_freq = 0, 0
+        for label, freq in d:
+            if freq < display_threshold:
+                ignored_count += 1
+                ignored_freq += freq
+            else:
+                yield label, freq
+            #end if
+        #end for
+
+        if ignored_count > 0:
+            yield 'Ignored labels with freq < {}'.format(display_threshold), '{} ({} labels)'.format(ignored_freq, ignored_count)
+    #end def
+
     total_count = 0
     labels_freq = Counter()
     for f in instance_files:
@@ -52,14 +71,14 @@ def load_instances(instance_files, labels_field='labels', limit=None, progress_i
 
         if labels_field:
             labels_freq += freq
-            logger.info('Label frequencies for <{}>:\n{}'.format(f.name, tabulate(freq.most_common() + [('Labels total', sum(freq.values())), ('Total', total_count)], headers=('Label', 'Freq'), tablefmt='psql')))
+            logger.info('Label frequencies for <{}>:\n{}'.format(f.name, tabulate(list(_filter_display(freq.most_common())) + [('Labels total', sum(freq.values())), ('Total', total_count)], headers=('Label', 'Freq'), tablefmt='psql')))
         #end if
 
         if limit and total_count >= limit: break
     #end for
 
     if labels_field and len(instance_files) > 1:
-        logger.info('Total label frequencies:\n{}'.format(tabulate(labels_freq.most_common() + [('Labels total', sum(labels_freq.values())), ('Total', total_count)], headers=('Label', 'Freq'), tablefmt='psql')))
+        logger.info('Total label frequencies:\n{}'.format(tabulate(list(_filter_display(labels_freq.most_common())) + [('Labels total', sum(labels_freq.values())), ('Total', total_count)], headers=('Label', 'Freq'), tablefmt='psql')))
 #end def
 
 
