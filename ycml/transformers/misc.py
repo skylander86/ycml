d71009ed1327feb81bec656aa7533738564856fe
yc <yanchuan@outlook.com>
2017-07-12 15:57:46 +0800

    Add `DictListExtractionTransformer`

--- ycml/transformers/misc.py
+++ ycml/transformers/misc.py
@@ -1,4 +1,4 @@
-__all__ = ['DictExtractionTransformer']
+__all__ = ['DictExtractionTransformer', 'DictListExtractionTransformer']
 
 from . import PureTransformer
 
@@ -29,4 +29,33 @@ class DictExtractionTransformer(PureTransformer):
     #end def
 
     def transform_one(self, d): return d.get(self.key, self.default)
+#end class
+
+
+class DictListExtractionTransformer(PureTransformer):
+    """Extract a given key from list of dictionary object."""
+
+    def __init__(self, key=None, default=None, **kwargs):
+        super(DictListExtractionTransformer, self).__init__(**kwargs)
+        self.set_params(key=key, default=default)
+    #end def
+
+    def get_params(self, deep=True):
+        params = super(DictListExtractionTransformer, self).get_params(deep=deep)
+        params['key'] = getattr(self, 'key', None)
+        params['default'] = getattr(self, 'default', None)
+
+        return params
+    #end def
+
+    def set_params(self, **kwargs):
+        super(DictListExtractionTransformer, self).set_params(**kwargs)
+
+        self.key = kwargs.pop('key', None)
+        self.default = kwargs.pop('default', None)
+
+        return self
+    #end def
+
+    def transform_one(self, L): return [d.get(self.key, self.default) for d in L]
 #end class
