66d6a9df56915cec2eb82177e71f3c2e86a6219d
yc <yanchuan@outlook.com>
2017-07-21 14:28:54 +0800

    Fix numerous bugs and handle multi labels

--- ycml/scripts/partition_instances.py
+++ ycml/scripts/partition_instances.py
@@ -3,8 +3,11 @@ This script is different from `ycml.scripts.partition_lines` because it takes la
 """
 
 from argparse import ArgumentParser
+from collections import Counter
 import json
 import logging
+
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 
@@ -27,19 +30,39 @@ def main():
 
     X, Y_labels = zip(*load_instances(A.instances, labels_field=A.label_key))
 
-    X_train, X_test = train_test_split(X, train_size=A.train_size, stratify=Y_labels)
+    labels_freq = Counter(label for labels in Y_labels for label in labels)
+
+    if any(len(labels) > 1 for labels in Y_labels):
+        logger.warn('These are multilabel instances, we will select the most common label for each instance to do stratification.')
+        Y_single_labels = [max(labels, key=lambda l: labels_freq[l]) for labels in Y_labels]
+    else:
+        Y_single_labels = Y_labels
+    #end for
+
+    labels_freq = Counter(Y_single_labels)
+    removed_labels = []
+    for i, label in enumerate(Y_single_labels):
+        if labels_freq[label] < 2:
+            removed_labels.append(label)
+            Y_single_labels[i] = '<none>'
+        #end if
+    #end for
+    removed_labels.sort()
+    logger.warning('The following labels were replaced with `<none>` for the purpose of stratification: {}.'.format(', '.join(removed_labels)))
+
+    X_train, X_test = train_test_split(X, train_size=A.train_size, stratify=Y_single_labels)
 
     for o in X_train:
         A.output[0].write(json.dumps(o))
         A.output[0].write('\n')
     #end for
-    logger.info('{} training instances written to <{}>.'.format(A.output[0].name))
+    logger.info('{} training instances written to <{}>.'.format(len(X_train), A.output[0].name))
 
     for o in X_test:
         A.output[1].write(json.dumps(o))
         A.output[1].write('\n')
     #end for
-    logger.info('{} evaluation instances written to <{}>.'.format(A.output[1].name))
+    logger.info('{} evaluation instances written to <{}>.'.format(len(X_test), A.output[1].name))
 #end def
 
 
