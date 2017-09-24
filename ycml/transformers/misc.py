4eca7828606b79010cf2fd52317ed84a0c7cf4eb
yc <yanchuan@outlook.com>
2017-09-24 16:33:48 +0800

    Clean yp `DictExtractionTransformer` code

--- ycml/transformers/misc.py
+++ ycml/transformers/misc.py
@@ -7,25 +7,11 @@ class DictExtractionTransformer(PureTransformer):
     """Extract a given key from dictionary object."""
 
     def __init__(self, key=None, default=None, **kwargs):
+        kwargs.setdefault('nparray', False)
         super(DictExtractionTransformer, self).__init__(**kwargs)
-        self.set_params(key=key, default=default)
-    #end def
 
-    def get_params(self, deep=True):
-        params = super(DictExtractionTransformer, self).get_params(deep=deep)
-        params['key'] = getattr(self, 'key', None)
-        params['default'] = getattr(self, 'default', None)
-
-        return params
-    #end def
-
-    def set_params(self, **kwargs):
-        super(DictExtractionTransformer, self).set_params(**kwargs)
-
-        self.key = kwargs.pop('key', None)
-        self.default = kwargs.pop('default', None)
-
-        return self
+        self.key = key
+        self.default = default
     #end def
 
     def transform_one(self, d, **kwargs): return d.get(self.key, self.default)
